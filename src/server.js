bc022f2f20d56cf3245a61ad997b7c1b35bc1184
Robin La Rondelle <ra.larondelle@student.avans.nl>
2019-02-05 13:53:48 +0100

    Changed the way environment variables are handled

--- src/server.js
+++ src/server.js
@@ -1,30 +1,36 @@
-if(process.env.NODE_ENV === "production") require("dotenv").config({path: "../environment/prod.environment.env"})
-if(process.env.NODE_ENV === "development") require("dotenv").config({path: "../environment/dev.environment.env"})
+require("dotenv").config({path: "./environment/environment.env"}) //Get the environment file
+console.log("API running in " + process.env.NODE_ENV + " mode")
 
-const express = require('express')
+const express = require('express') //HTTP request framework
 const morgan = require("morgan")
-const bodyParser = require('body-parser')
-const passport = require("passport")
+const bodyParser = require('body-parser') //Pase request body to JSON
+const passport = require("passport") // Passport.js to secure the API
 
 const port = process.env.PORT || "3000"
 const app = express() 
 
-const authRoutes = require("./routes/auth.routes")
-
+// Setup express app
 app.use(bodyParser.json())
 app.use(morgan("dev"))
 app.use(passport.initialize())
 
+// Routes
+const authRoutes = require("./routes/auth.routes")
+
+// Endpoints
 app.use("/api", authRoutes)
 
+//Catch all non existing endpoints
 app.use("*", function (req, res, next) {
   next(new ApiError("Endpoint not found", 404, Date.now()))
 })
 
+//Error endpoint
 app.use((err, req, res, next) => {
   res.status(err.code || 404).json(err).send();
 })
 
+//Setup server on designated port
 app.listen(port, () => {
   console.log("Server is running on port: " + port)
 })
